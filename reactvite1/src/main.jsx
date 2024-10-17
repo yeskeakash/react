130d4d496d117399fb32690bcb961852a82f8ed0
yeskeakash <yeskeakash@gmail.com>
2024-10-17 21:01:10 +0530

    Revert "react basics"
    This reverts commit f5e6300ad4a24fe7f99b5658cdefe07939713fd1.

--- reactvite1/src/main.jsx
+++ reactvite1/src/main.jsx
@@ -1,32 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
-const reactElement = {
-  type : 'a',
-  props : { 
-      href : "https://youtube.com",
-      target : '_blank'
-  },
-  children : 'Click Here'
-}
-
-
-function MyApp(){
-  return(
-    <div>
-      <h1>Welcome to my website</h1>
-    </div>
-  )
-}
-
-const AnotherElement = (
-  <a href="https://youtube.com" target="_blank">YOUTUBE</a>
-)
-
-
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  // AnotherElement
-  <App/>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
